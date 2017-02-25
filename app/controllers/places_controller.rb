94bd2096b58e9669039cdf934528606d043dfa4c
roderickjoseph <rjgauvin@gmail.com>
2017-02-25 18:56:10 +0000

    Added validation for place name requiring it not be blank

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -33,12 +37,16 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @place = Place.find(params[:id])
-    
+
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
