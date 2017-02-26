5319d94bb9cfc63c44cd1251660e884c5d46eb3a
roderickjoseph <rjgauvin@gmail.com>
2017-02-26 05:16:12 +0000

    Added Bing API key for geocode

--- Gemfile
+++ Gemfile
@@ -66,3 +66,9 @@ gem "font-awesome-rails"
 
 # Devise - User Authentication
 gem 'devise'
+
+# Geocoder
+gem 'geocoder'
+
+# Figaro - Add Bing API key to ENV (environment variable)
+gem "figaro"
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  
+  validates :name, length: { minimum: 2 }, presence: true
+  validates :address, :description, presence: true
 end
