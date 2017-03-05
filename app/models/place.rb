89473310c288143821da3432c80349e98234be2a
roderickjoseph <rjgauvin@gmail.com>
2017-03-05 04:27:26 +0000

    Set up comment model and db connections

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, length: { minimum: 2 }, presence: true
   validates :address, :description, presence: true
 end
